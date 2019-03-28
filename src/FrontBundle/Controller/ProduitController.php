e3c4373d6628030d0ac866d05dc361bb4e95cbf4
farah <farah.abdelli@esprit.tn>
2019-03-28 14:44:55 +0100

    modification frontend #farah

--- src/FrontBundle/Controller/ProduitController.php
+++ src/FrontBundle/Controller/ProduitController.php
@@ -2,17 +2,15 @@
 
 namespace FrontBundle\Controller;
 
-use FrontBundle\Entity\Produits;
-use FrontBundle\Form\ProduitsType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+
 
 class ProduitController extends Controller
 {
     public function afficherCatalogueAction()
     {
         $em=$this->getDoctrine()->getManager();
-        $modeles=$em->getRepository('BackBundle:Catalogue')->findAll();
+        $modeles=$em->getRepository('FrontBundle:Produits')->findAll();
 
 
 
