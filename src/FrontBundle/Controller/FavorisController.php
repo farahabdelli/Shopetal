fe04cf61bceaf9c2dd13406acdcfed39d0393959
farah <farah.abdelli@esprit.tn>
2019-03-29 15:42:51 +0100

    crud Favoris frontend #farah

---
+++ src/FrontBundle/Controller/FavorisController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace FrontBundle\Controller;
+
+use FrontBundle\Entity\Favoris;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class FavorisController extends Controller
+{
+    public function afficherFavorisAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $modeles = $em->getRepository('FrontBundle:Favoris')->findAll();
+
+        return $this->render('@Front/Favoris/afficherFavoris.html.twig', array('emm' => $modeles));
+    }
+
+
+    public function addtofavorisAction( $id)
+    {
+        # Get object from doctrine manager
+        $fav = new Favoris();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $product = $this->getDoctrine()->getRepository('FrontBundle:Produits')
+            ->find($id);
+
+        $fav->setIdProduit($product->getId());
+        $fav->setNomProduit($product->getNom());
+        $fav->setImageProduit($product->getImage());
+        $fav->setPrixProduit($product->getPrix());
+
+
+        $em->persist($fav);
+        $em->flush();
+        return $this->redirectToRoute('afficherCatalogue');
+
+    }
+
+    public function supprimerFavorisAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $modele = $em->getRepository('FrontBundle:Favoris')->find($id);
+        $em->remove($modele);
+
+        $em->flush();
+        return $this->redirectToRoute('afficherFavoris');
+    }
+
+}
---
+++ src/FrontBundle/Entity/Favoris.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace FrontBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Favoris
+ *
+ * @ORM\Table(name="favoris")
+ * @ORM\Entity
+ */
+class Favoris
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_favoris", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idFavoris;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_produit", type="integer", nullable=false)
+     */
+    private $idProduit;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom_produit", type="string", length=30, nullable=false)
+     */
+    private $nomProduit;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image_produit", type="string", length=30, nullable=false)
+     */
+    private $imageProduit;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="prix_produit", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $prixProduit;
+
+    /**
+     * Favoris constructor.
+     * @param int $idFavoris
+     * @param int $idProduit
+     * @param string $nomProduit
+     * @param string $imageProduit
+     * @param float $prixProduit
+     */
+    public function Favoris($idFavoris, $idProduit, $nomProduit, $imageProduit, $prixProduit)
+    {
+        $this->idFavoris = $idFavoris;
+        $this->idProduit = $idProduit;
+        $this->nomProduit = $nomProduit;
+        $this->imageProduit = $imageProduit;
+        $this->prixProduit = $prixProduit;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdFavoris()
+    {
+        return $this->idFavoris;
+    }
+
+    /**
+     * @param int $idFavoris
+     */
+    public function setIdFavoris($idFavoris)
+    {
+        $this->idFavoris = $idFavoris;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdProduit()
+    {
+        return $this->idProduit;
+    }
+
+    /**
+     * @param int $idProduit
+     */
+    public function setIdProduit($idProduit)
+    {
+        $this->idProduit = $idProduit;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNomProduit()
+    {
+        return $this->nomProduit;
+    }
+
+    /**
+     * @param string $nomProduit
+     */
+    public function setNomProduit($nomProduit)
+    {
+        $this->nomProduit = $nomProduit;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImageProduit()
+    {
+        return $this->imageProduit;
+    }
+
+    /**
+     * @param string $imageProduit
+     */
+    public function setImageProduit($imageProduit)
+    {
+        $this->imageProduit = $imageProduit;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrixProduit()
+    {
+        return $this->prixProduit;
+    }
+
+    /**
+     * @param float $prixProduit
+     */
+    public function setPrixProduit($prixProduit)
+    {
+        $this->prixProduit = $prixProduit;
+    }
+
+
+
+}
+
