a2c9e16bbe0048f87fe15a8547b34f4725559716
farah <farah.abdelli@esprit.tn>
2019-04-14 00:17:16 +0100

    metier #farah

--- src/FrontBundle/Controller/PanierController.php
+++ src/FrontBundle/Controller/PanierController.php
@@ -136,11 +136,7 @@ class PanierController extends Controller
         $notif = $manager->createNotification('Hello world !');
         $notif->setMessage('This a notification.');
         $notif->setLink('http://symfony.com/');
-        // or the one-line method :
-        // $manager->createNotification('Notification subject','Some random text','http://google.fr');
 
-        // you can add a notification to a list of entities
-        // the third parameter `$flush` allows you to directly flush the entities
         $manager->addNotification(array($this->getUser()), $notif, true);
 
     }
